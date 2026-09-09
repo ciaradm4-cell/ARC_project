00f7c5329ce71034ea7883db89b035a9cdbddd9b
ciaradm4-cell <ciaradm4@gmail.com>
2026-09-09 15:59:39 +0100

    wrapping executable code in a function to fix multiprocessing
    Moved worker function and multiprocessing into a function main() for correct structure.

--- phot_pipeline_withfuncs.py
+++ phot_pipeline_withfuncs.py
@@ -50,35 +50,38 @@ def parse_args():
     )
  
     return parser.parse_args()
+
+def main():
+ args = parse_args()
+  
+ # specify field and campaign/subdir which is set from the command line instead of hardcoded
+ field_name = args.field_name
+ campaign_name = args.campaign_name
+  
+ # setting the number of cores to use 
+ num_workers = args.num_workers
  
-args = parse_args()
+ # get necessary information about field and objects  (list of all actions, source names, and their xy positions)
+ actions, obj_names, source_id, x, y = field_info(field_name, campaign_name)
  
-# specify field and campaign/subdir which is set from the command line instead of hardcoded
-field_name = args.field_name
-campaign_name = args.campaign_name
+ # allows for the worker to have just one varyiing argument which is the action and keeps the rest const (fills the other 5 arguments the same each time)
+ def worker(action):
+     return action_photometry(action, source_id=source_id, x=x, y=y, field_name=field_name, campaign_name=campaign_name)
  
-# setting the number of cores to use 
-num_workers = args.num_workers
+ # splits the actions list across num_workers processes which each calls worker(action)
+ with multiprocessing.Pool(processes=num_workers) as pool:
+     pool.map(worker, actions)   # worker is the function called to work on a given action which is given from the list of actions supplied
+ 
+ field_campaign_flux_data = concatenate_actions(actions, field_name, campaign_name)     # returns the dataframe of flux data for the entire field campaign
+ 
+ # plots raw and corrected light curve for each candidate object (visually inspect these to see which ones are actual objects)
+ for obj in obj_names:
+     plot_lightcurve(obj, field_campaign_flux_data, field_name, campaign_name)
+     mjd, corrected_flux, corrected_flux_err, disc_mjd = comp_norm(obj, field_name, campaign_name)
+     plot_compnorm_lc(mjd, corrected_flux, disc_mjd, obj, field_name, campaign_name)
+     plot_binned_lc(mjd, corrected_flux, disc_mjd, obj, field_name, campaign_name)
+ # could also get this to eventually run over all campaigns present in a field and concatenate them at the end to plot a full field light curve
 
-# get necessary information about field and objects  (list of all actions, source names, and their xy positions)
-actions, obj_names, source_id, x, y = field_info(field_name, campaign_name)
-
-# allows for the worker to have just one varyiing argument which is the action and keeps the rest const (fills the other 5 arguments the same each time)
-def worker(action):
-    return action_photometry(action, source_id=source_id, x=x, y=y, field_name=field_name, campaign_name=campaign_name)
-
-# splits the actions list across num_workers processes which each calls worker(action)
-with multiprocessing.Pool(processes=num_workers) as pool:
-    pool.map(worker, actions)   # worker is the function called to work on a given action which is given from the list of actions supplied
-
-field_campaign_flux_data = concatenate_actions(actions, field_name, campaign_name)     # returns the dataframe of flux data for the entire field campaign
-
-# plots raw and corrected light curve for each candidate object (visually inspect these to see which ones are actual objects)
-for obj in obj_names:
-    plot_lightcurve(obj, field_campaign_flux_data, field_name, campaign_name)
-    mjd, corrected_flux, corrected_flux_err, disc_mjd = comp_norm(obj, field_name, campaign_name)
-    plot_compnorm_lc(mjd, corrected_flux, disc_mjd, obj, field_name, campaign_name)
-    plot_binned_lc(mjd, corrected_flux, disc_mjd, obj, field_name, campaign_name)
-# could also get this to eventually run over all campaigns present in a field and concatenate them at the end to plot a full field light curve
-
+if __name__ == "__main__":
+    main()
 
